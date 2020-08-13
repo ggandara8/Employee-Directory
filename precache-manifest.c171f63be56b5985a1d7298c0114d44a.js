7d0a61297dd6bdeb631d84535693d3d2462550e5
ggandara8 <ggandara8@gmail.com>
2020-08-13 18:13:26 -0500

    Updates

---
+++ precache-manifest.c171f63be56b5985a1d7298c0114d44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd757bb325896149417b9990d4ebe050",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "837f63b1f9a44ac1ea3a",
+    "url": "/Employee-Directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1b23edd1ba8e3448cc9",
+    "url": "/Employee-Directory/static/js/2.c8a54ef7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Employee-Directory/static/js/2.c8a54ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837f63b1f9a44ac1ea3a",
+    "url": "/Employee-Directory/static/js/main.e3d374b5.chunk.js"
+  },
+  {
+    "revision": "384a0ee110d8abbcc684",
+    "url": "/Employee-Directory/static/js/runtime-main.c4106856.js"
+  }
+]);
